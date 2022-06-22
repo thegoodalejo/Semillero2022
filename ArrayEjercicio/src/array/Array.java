cd01551c7e9d404ff1cc539731f31117c90cc7d2
Juan Mora <jpmoratres@gmail.com>
2022-06-22 11:30:23 -0500

    Array odenado de mayor a menor y de menor a mayo JUAN P. MORA

--- ArrayEjercicio/src/array/Array.java
+++ ArrayEjercicio/src/array/Array.java
@@ -5,25 +5,44 @@ public class Array {
 	public static void main(String[] args) {
 		
        int[] Array_numeros = {5,7,2,4,1};
-       int temporal ;
+       int numeroActual,numeroSiguiente ;
        System.out.println("Mi arreglo de numeros es: ");
        for (int i=0; i<Array_numeros.length; i++) {
     	   System.out.println(Array_numeros[i]);
        }
        
    	//Usando condicionales y ciclos organice de mayor a menor
-       System.out.println("Mi arreglo Ordenado de mayor a menor: ");
+    
        for (int i=0; i<Array_numeros.length; i++) {
-    	   
-    	   for (int j=0; j<Array_numeros.length-i-1; j++) {
-    		   if (Array_numeros[i]<Array_numeros[i+1]){
-    			   temporal = Array_numeros[i+1]; 
-    			   Array_numeros[i+1] = Array_numeros[i];
-    			   Array_numeros[i]= temporal;
+    	   for (int j=0; j<Array_numeros.length-1; j++) {
+    		   numeroActual = Array_numeros[j];
+    		   numeroSiguiente =  Array_numeros[j+1];
+    		   if (numeroActual<numeroSiguiente){
+    			   Array_numeros[j]=numeroSiguiente;
+    			   Array_numeros[j+1]=numeroActual;
     		   }
     	   }
-    	  
-    	      
+       }
+  
+       System.out.println("Mi arreglo Ordenado de mayor a menor: ");
+       for (int i=0; i<Array_numeros.length; i++) {
+    	   System.out.println(Array_numeros[i]);
+       }
+
+      	//Usando condicionales y ciclos organice de menor a mayor
+       
+       for (int i=0; i<Array_numeros.length; i++) {
+    	   for (int j=0; j<Array_numeros.length-1; j++) {
+    		   numeroActual = Array_numeros[j];
+    		   numeroSiguiente =  Array_numeros[j+1];
+    		   if (numeroActual>numeroSiguiente){
+    			   Array_numeros[j]=numeroSiguiente;
+    			   Array_numeros[j+1]=numeroActual;
+    		   }
+    	   }
+       }
+       System.out.println("Mi arreglo Ordenado de menor a mayor: ");
+       for (int i=0; i<Array_numeros.length; i++) {
     	   System.out.println(Array_numeros[i]);
        }
 	}
