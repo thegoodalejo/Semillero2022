50a8f4118e5c3902271b83b523a876f56b6fea88
Juan Mora <jpmoratres@gmail.com>
2022-06-23 18:16:41 -0500

    clases cafe: solo, dobel, americano

--- MaquinaCafe/src/cafe/CafeAmericano.java
+++ MaquinaCafe/src/cafe/CafeAmericano.java
@@ -7,5 +7,31 @@ public class CafeAmericano extends Cafe{
 		// TODO Auto-generated constructor stub
 	}
 
-
+public void cuenta(CafeAmericano obj) {
+		
+		System.out.println("Gracias por tu compra, vuelve pronto:");
+		System.out.println("*************************************");
+		System.out.println("Tu Cafe Americano:");
+		System.out.println("Tamaño de Vaso:"+obj.getTamanoVaso());
+		if(obj.getAzucar()==1) {
+			System.out.println("Azucar:SI");
+		}else {
+			System.out.println("Azucar:NO");
+		}
+		if(obj.getPalito()==1) {
+			System.out.println("Pitillo:SI");
+		}else {
+			System.out.println("Pitillo:NO");
+		}
+		if(obj.getServir()==1) {
+			System.out.println("Para llevar:SI");
+		}else {
+			System.out.println("Para llevar:NO");
+		}
+		System.out.println("Porcentaje de Agua:"+obj.getAgua());
+		System.out.println("Porcentaje de Cafe:"+obj.getCafe());
+		System.out.println("El total de tu compra es:"+obj.getPrecio()+" pesos");
+		System.out.println("*************************************");
+		
+	}
 }
--- MaquinaCafe/src/cafe/Main.java
+++ MaquinaCafe/src/cafe/Main.java
@@ -10,14 +10,17 @@ public class Main {
 
         int opcion=0;
         
-        CafeConLeche cafeConleche = new CafeConLeche("",0,0,0,5000,0,0,50);
+        CafeConLeche cafeConleche = new CafeConLeche("",0,0,0,6000,0,50,50);
+        CafeDoble cafedoble = new CafeDoble("",0,0,0,4000,0,100);
+        CafeSolo cafesolo = new CafeSolo("",0,0,0,2500,70,30);
+        CafeAmericano cafeamericano = new CafeAmericano("",0,0,0,4500,50,50);
 
         System.out.println("Maquina de Cafe");
         System.out.println("seleccione sus opciones.");
 
         do {
             System.out.println("1 - Cafe Con leche");
-            System.out.println("2 - Cafe Doble Crema");
+            System.out.println("2 - Cafe Doble");
             System.out.println("3 - Cafe Sencillo");
             System.out.println("4 - Cafe Americano");
             System.out.println("5 - Salir");
@@ -38,18 +41,39 @@ public class Main {
                     break;
 
                 case 2:
-
+                	
+                	cafedoble.setTamanoVaso(cafedoble.escogerVaso());
+                	cafedoble.setPalito(cafedoble.escogerPalito());
+                	cafedoble.setAzucar(cafedoble.escogerAzucar());
+                	cafedoble.setServir(cafedoble.escogerServicio());
+                    
+                	cafedoble.cuenta(cafedoble);
+                	opcion=5;
                    
                     break;
 
                 case 3:
 
+                	cafesolo.setTamanoVaso(cafesolo.escogerVaso());
+                	cafesolo.setPalito(cafesolo.escogerPalito());
+                	cafesolo.setAzucar(cafesolo.escogerAzucar());
+                	cafesolo.setServir(cafesolo.escogerServicio());
+                    
+                	cafesolo.cuenta(cafesolo);
+                	opcion=5;
       
                     break;
 
 
                 case 4:
-
+                	
+                	cafeamericano.setTamanoVaso(cafeamericano.escogerVaso());
+                	cafeamericano.setPalito(cafeamericano.escogerPalito());
+                	cafeamericano.setAzucar(cafeamericano.escogerAzucar());
+                	cafeamericano.setServir(cafeamericano.escogerServicio());
+                    
+                	cafeamericano.cuenta(cafeamericano);
+                	opcion=5;
 
                     break;
 
