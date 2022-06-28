22dce3ed10314df6b22ae62d424f99f614a6a74a
Juan Esteban Henao Ramirez <juan.henaor@sophossolutions.com>
2022-06-28 10:37:32 -0500

    Cambio de archivos a MiTrabajo

---
+++ MiTrabajo/PracticaPOO/src/practica1/Salon.java
@@ -0,0 +1,18 @@
+package practica1;
+
+public class Salon {
+	
+	int piso;
+	int numero;
+	int contador;
+	
+	public Salon(int piso, int numero) {
+		this.piso = piso;
+		this.numero = numero;
+	}
+	
+	
+	public void ingresar() {
+		contador++;
+	}
+}
