b0450ee0528c668433674e6912d1e4fc6f1fa42b
Ivan Dario Rios Ariza <ivan.riosa@sophossolutions.com>
2022-06-29 15:49:59 -0500

    290622 - Ivan Rios: Realización del ejercicio armstrong

---
+++ MiTrabajoIvanRios/Revisar/PracticasJava/src/com/armstrong/Armstrong.java
@@ -0,0 +1,54 @@
+package com.armstrong;
+
+import java.util.*;
+
+public class Armstrong {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a, b;
+		Scanner leer = new Scanner(System.in);
+		System.out.println("Listado de numeros Armstrong");
+		System.out.println("Ingrese valor inicial: ");
+		a = leer.nextInt();
+		System.out.println("Ingrese valor final: ");
+		b = leer.nextInt();
+		if (a < 0 || b < 0) {
+			System.out.println("Los numeros deben ser enteros positivos mayores a cero (0)");
+		} else {
+			if (b <= a) {
+				System.out.println("El valor final debe ser mayor al inicial");
+			} else {
+				System.out.println("Listado de numeros Armstrong");
+				for (int numero = a;numero<=b;numero++) {
+					int suma = 0;
+					ArrayList<Integer> numeroSeparado = new ArrayList<Integer>();
+					numeroSeparado = descomponerNumero(numero);
+					for (int n : numeroSeparado) {
+						suma = suma + calcularCubo(n);
+					}
+	
+					if (suma == numero) {
+						System.out.println(numero);
+					}
+				}
+			}
+
+		}
+
+	}
+
+	public static ArrayList<Integer> descomponerNumero(int n) {
+		ArrayList<Integer> numero = new ArrayList<Integer>();
+		String numeroString = String.valueOf(n);
+		for (int i = 0; i < numeroString.length(); i++) {
+			numero.add(Integer.parseInt(String.valueOf(numeroString.charAt(i))));
+		}
+		return numero;
+	}
+
+	public static int calcularCubo(int n) {
+		return n * n * n;
+	}
+
+}
