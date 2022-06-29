a736abfdde274e24a511e22005ceea42466e66a0
Juan Mora <jpmoratres@gmail.com>
2022-06-28 22:59:10 -0500

    Se agrega validacion capacidad de area

--- MiTrabajoJuanPabloMora/Practica1POO/src/fabrica/Area.java
+++ MiTrabajoJuanPabloMora/Practica1POO/src/fabrica/Area.java
@@ -10,6 +10,7 @@ public class Area {
 	int capacidad;
 	int contador=0;
 	ArrayList<Equipo> listaEquipos = new ArrayList<Equipo>();
+	ArrayList<Equipo> listaEquiposAtrasados = new ArrayList<Equipo>();
 
 	
 	public Area(int division, int area, String descripcion, int capacidad) {
@@ -22,14 +23,27 @@ public class Area {
 	
 	public void ingresar(Equipo equipo) {
 		contador++;
-		System.out.println("Ingreso:  "+equipo.nombre+" --- Total Equipos: "+contador);
-		listaEquipos.add(equipo);
+		System.out.println("Ingreso:  "+equipo.nombre+", Serie:"+equipo.serie+", Area: "+descripcion+", ---Total : "+contador);
+		
+		if (contador<=this.capacidad) {
+		    listaEquipos.add(equipo);
+		}else {
+			listaEquiposAtrasados.add(equipo);
+		}
 	}
 	
 	public void enProceso() {
-		System.out.println("Equipos en Proceso: ");		
+		System.out.println("");
+		System.out.println("Equipos en Proceso: ");
+		
 		for(Equipo equipo : listaEquipos) {
-			System.out.println(equipo.nombre);
+			System.out.println("Nombre: "+equipo.nombre+" Serie: "+equipo.serie+" Ubicacion: Division "+division+" area "+area);
+		}
+		System.out.println("");
+		System.out.println("Equipos Resagados/Atrasados: ");
+		
+		for(Equipo equipo : listaEquiposAtrasados) {
+			System.out.println("Nombre: "+equipo.nombre+" Serie: "+equipo.serie+" Ubicacion Bodega: Division "+division+" area "+area);
 		}
 	}
 	
--- MiTrabajoJuanPabloMora/Practica1POO/src/fabrica/Main.java
+++ MiTrabajoJuanPabloMora/Practica1POO/src/fabrica/Main.java
@@ -30,8 +30,10 @@ public class Main {
 
 		//
 		
-		Ensamble ensamble = new Ensamble(1,3,"Ensamble de equipos Gama Alta",50);
-		Testeo testeo = new Testeo(5,4,"Testeo de equipos Gama Alta",20);
+		Ensamble ensamble = new Ensamble(1,3,"Ensamble de equipos Gama Alta",5);
+		Testeo testeo = new Testeo(5,4,"Testeo de equipos Gama Alta",3);
+		
+		
 		
 		System.out.println("");
 		System.out.println("*******************************Ingreso de equipos al Area de ENSAMBLE*******************************");
@@ -40,13 +42,15 @@ public class Main {
 		ensamble.ingresar(celular1);
 		ensamble.ingresar(consola4);
 		ensamble.ingresar(consola5);
-		
+		ensamble.ingresar(celular3);
+		ensamble.ingresar(celular2);
 		System.out.println("");
 		System.out.println("*******************************Ingreso de equipos al Area de TESTEO*******************************");
 		testeo.ingresar(consola1);
 		testeo.ingresar(celular1);
 		testeo.ingresar(celular2);
 		testeo.ingresar(celular1);
+		testeo.ingresar(consola5);
 		
 		System.out.println("");
 		System.out.println("*******************************Proceso Actual de Ensamble*******************************");
