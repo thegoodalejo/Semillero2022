6b60afd08365ce90eb2bf7face7f2e1c80d4c75e
JuanMora <jpmoratres@gmail.com>
2022-06-30 13:18:12 -0500

    30/06/2022, Equipo3, Creacion clases padre e hijas

--- PracticaJava/src/fabrica/Calzado.java
+++ PracticaJava/src/fabrica/Calzado.java
@@ -1,15 +1,90 @@
 package fabrica;
 
-public class Calzado {
+public abstract class  Calzado {
 	
 	private String marca;
 	private String material;
 	private int talla;
-	private String tipo;
 	private String tipoSuela;
 	private String colorBase;
 	private boolean cordon;
 	private String genero;
+	
+	
+	
+	public Calzado() {
+		super();
+	}
+
+	public Calzado(String marca, String material, int talla, String tipoSuela, String colorBase, boolean cordon,
+			String genero) {
+		super();
+		this.marca = marca;
+		this.material = material;
+		this.talla = talla;
+		this.tipoSuela = tipoSuela;
+		this.colorBase = colorBase;
+		this.cordon = cordon;
+		this.genero = genero;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getMaterial() {
+		return material;
+	}
+
+	public void setMaterial(String material) {
+		this.material = material;
+	}
+
+	public int getTalla() {
+		return talla;
+	}
+
+	public void setTalla(int talla) {
+		this.talla = talla;
+	}
+
+	public String getTipoSuela() {
+		return tipoSuela;
+	}
+
+	public void setTipoSuela(String tipoSuela) {
+		this.tipoSuela = tipoSuela;
+	}
+
+	public String getColorBase() {
+		return colorBase;
+	}
+
+	public void setColorBase(String colorBase) {
+		this.colorBase = colorBase;
+	}
+
+	public boolean isCordon() {
+		return cordon;
+	}
+
+	public void setCordon(boolean cordon) {
+		this.cordon = cordon;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	
+	
 
 	
 
---
+++ PracticaJava/src/fabrica/Deportivo.java
@@ -0,0 +1,15 @@
+package fabrica;
+
+public class Deportivo extends Calzado {
+	
+	public Deportivo() {}
+
+	public Deportivo(String marca, String material, int talla, String tipoSuela, String colorBase, boolean cordon,
+			String genero) {
+		super(marca, material, talla, tipoSuela, colorBase, cordon, genero);
+
+	}
+	
+	
+
+}
