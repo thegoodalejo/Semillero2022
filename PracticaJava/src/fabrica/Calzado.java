e41f844433d92a77a4eec888bd049135d42aa448
JuanMora <jpmoratres@gmail.com>
2022-06-30 12:28:13 -0500

    30/06/2022, Equipo3, Creacion poryecto y clase padre

---
+++ PracticaJava/src/fabrica/Calzado.java
@@ -0,0 +1,16 @@
+package fabrica;
+
+public class Calzado {
+	
+	private String marca;
+	private String material;
+	private int talla;
+	private String tipo;
+	private String tipoSuela;
+	private String colorBase;
+	private boolean cordon;
+	private String genero;
+
+	
+
+}
