d859619b414c9a3f9b0bdfe110c5abe995c931a3
Mario Alejandro Garcia Cantillo <mario.garcia@sophossolutions.com>
2022-06-30 20:45:32 -0500

    30062022-MarioGarcia:Corrección Ejercicio numero de Amstrong

--- MiTrabajoMarioAlejandroGarcia/Revisar/Ejercicios_logica/src/Amstrong/NumeroAmstrong.java
+++ MiTrabajoMarioAlejandroGarcia/Revisar/Ejercicios_logica/src/Amstrong/NumeroAmstrong.java
@@ -9,30 +9,48 @@ public class NumeroAmstrong {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Ingrese el primer numero: ");
 		int numA = sc.nextInt();
-		System.out.println("Ingrese el primer numero: ");
+		System.out.println("Ingrese el segundo numero: ");
 		int numB = sc.nextInt();
 		
-		if(numA > 0 && numB > 0) {
-			if(numA < numB) {
-				for(int i=numA; i<numB;i++) {
-					int numAms=0;
-					int digito =0;
-					int sumaDig = 0;
-					numAms = i;
-					while(numAms != 0) {
-						digito = numAms % 10;
-						sumaDig = sumaDig + digito*digito+digito;
-						numAms /= 10;
+		//Validamos que los numA y numB sean enteros positivos
+		if(numA>0 && numB>0) {
+			//Validamos que numA es menor que NumB
+			if(numB>numA) {
+				System.out.println("Los numeros de Amstrong entre "+numA+" y "+numB+" son: ");
+				for(int i=numA; i < numB; i++) {
+					if (calcularNArms(i)) {
+						 System.out.println(i);
 					}
-					if(sumaDig == i) {
-						System.out.println(i);
-					}
-				}
-				
+		        }	
 			}
 		}
-		
+    }
 
-	}
-
+    public static boolean calcularNArms(int num) {
+        int contDig = 0; 
+        int sumDig = 0;
+        int aux = num;
+        int ultDig = 0;
+        
+       //Realizamos el cálculo del número de digitos
+        while (aux != 0) {
+            aux /= 10;
+            contDig++;
+        }
+        ultDig = num;
+        
+        //Realizamos el calculo del suma de los digitos de num elevados a la n-esima potencia
+        while (ultDig != 0) {
+            int residuo = ultDig % 10;
+            sumDig += Math.pow(residuo, contDig);
+            ultDig /= 10;
+        }
+        //Validamos  que num es número de Amstrong si se cumple que la suma de los cubos de los digitos es igual al num
+        //si es verdadero retorna true, sino retorna false
+        if (sumDig == num) 
+        {
+        	return true;
+        }
+        return false;
+    }
 }
