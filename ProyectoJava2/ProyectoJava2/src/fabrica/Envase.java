1d0dd895b3b30ced5313acb8be5479e9c56aa252
Ivan Dario Rios Ariza <ivan.riosa@sophossolutions.com>
2022-07-01 08:52:20 -0500

    010722-IvanRios: Se crean los ArrayList para Fragancia y envase

--- ProyectoJava2/ProyectoJava2/src/fabrica/Envase.java
+++ ProyectoJava2/ProyectoJava2/src/fabrica/Envase.java
@@ -1,11 +1,17 @@
 package fabrica;
 
+import java.util.ArrayList;
+
 public class Envase {
 	public String nombre;
 	public int tamanio;
+	public static ArrayList<Envase> lstEnvases = new ArrayList<Envase>();
+	
 	
 	public Envase(String nombre, int tamanio) {
 		this.nombre = nombre;
 		this.tamanio = tamanio;
+		lstEnvases.add(this);
+		
 	}
 }
--- ProyectoJava2/ProyectoJava2/src/fabrica/Fragancia.java
+++ ProyectoJava2/ProyectoJava2/src/fabrica/Fragancia.java
@@ -1,12 +1,17 @@
 package fabrica;
 
+import java.util.ArrayList;
+
 public class Fragancia {
 	public String nombre;
 	public Genero genero;
+	public static ArrayList<Fragancia> lstFragancias = new ArrayList<Fragancia>();
+	
 	
 	public Fragancia(String nombre,Genero genero) {
 		this.nombre = nombre;
 		this.genero = genero;
+		lstFragancias.add(this);
 	}
 
 }
