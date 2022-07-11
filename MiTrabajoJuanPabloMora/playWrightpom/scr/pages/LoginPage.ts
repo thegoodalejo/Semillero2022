7bac976d0dc5a390b1bea67a5364d85900065691
JuanMora <jpmoratres@gmail.com>
2022-07-11 11:23:15 -0500

    11/07/2022, Juan P. Mora, Primera practica playWrightporm

---
+++ MiTrabajoJuanPabloMora/playWrightpom/scr/pages/LoginPage.ts
@@ -0,0 +1,28 @@
+import { Page } from "@playwright/test";
+
+
+export class LoginPage{
+
+    readonly page: Page;
+
+    constructor(page:Page){
+        this.page = page;
+    }
+
+    async logIn(){
+
+    // Fill input[name="userName"]
+    await this.page.locator('input[name="userName"]').fill('juan');
+    // Fill input[name="password"]
+    await this.page.locator('input[name="password"]').fill('1234');
+    // Click text=Submit
+    await this.page.locator('text=Submit').click();
+
+    }
+
+    async navegate(){
+      // Go to https://demo.guru99.com/test/newtours/
+      await this.page.goto('https://demo.guru99.com/test/newtours/');
+    }
+
+    }
