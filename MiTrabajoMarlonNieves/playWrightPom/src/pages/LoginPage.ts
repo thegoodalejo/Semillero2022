995c3eb1ad51d36e5b88e1c7be451207c4c60059
marlon-nieves <marlon.3801711612@ucaldas.edu.co>
2022-07-12 16:00:55 -0500

    Fix Chanfe: Locator end add IsLogIn

--- MiTrabajoMarlonNieves/playWrightPom/src/pages/LoginPage.ts
+++ MiTrabajoMarlonNieves/playWrightPom/src/pages/LoginPage.ts
@@ -1,31 +1,34 @@
-import { Page } from "@playwright/test";
+import { expect, Page } from "@playwright/test";
 
 
 
 
-export class LoginPage{
+export class LoginPage {
     readonly page: Page;
-    constructor(page: Page){
+    constructor(page: Page) {
         this.page = page;
     }
-    async LogIn(){
-         // Fill input[name="userName"]
-         await this.page.locator('input[name="userName"]').fill('admin');
-         // Fill input[name="password"]
-         await this.page.locator('input[name="password"]').fill('admin');
+    async LogIn(userName: string, password: string) {
+        const txtuserName = this.page.locator('input[name="userName"]');
+        //const txtpassword = 'input[name="userName"]';
+
+        // Fill input[name="userName"]
+        await txtuserName.fill(userName);
+
+        // Fill input[name="password"]
+        //await this.page.locator.fill(txtpassword, password);
+        await this.page.locator('input[name="password"]').fill(password);
 
         // Click text=Submit
         await this.page.locator('text=Submit').click();
-
-        //locator mapeo del elemento y luego mando el texto
-        //await this.page.locator('').fill('');
-        //otra forma mando elemento y texto en ua linea
-        //await this.page.fill('','');
     }
 
-    async navigate(){
+    async navigate() {
         // Go to https://demo.guru99.com/test/newtours/
         await this.page.goto('https://demo.guru99.com/test/newtours/');
 
+    }
+    async isLogIn() {
+        await expect(this.page.locator('text=Login Successfully'), 'should be loggen in').toBeVisible();
     }
 }
--- MiTrabajoMarlonNieves/playWrightPom/tests/loginSuccess.spec.ts
+++ MiTrabajoMarlonNieves/playWrightPom/tests/loginSuccess.spec.ts
@@ -5,8 +5,9 @@ test(`
 Given I was in the login Page
 When I was login in
 Then I saw the message login succesfull`,
-async ({ page }) => {
-  const loginPage = new LoginPage(page);
-  await loginPage.navigate();
-  await loginPage.LogIn();
-});
+  async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    await loginPage.navigate();
+    await loginPage.LogIn('admin', 'admin');
+    await loginPage.isLogIn();
+  });
