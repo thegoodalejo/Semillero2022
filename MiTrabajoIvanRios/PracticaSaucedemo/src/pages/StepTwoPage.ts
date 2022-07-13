180bf398805d607fe338693a04bec024b3663bf5
Ivan Dario Rios Ariza <ivan.riosa@sophossolutions.com>
2022-07-13 16:43:16 -0500

    Se crea el proyecto Sccrenplay de prueba

--- MiTrabajoIvanRios/PracticaSaucedemo/src/pages/StepTwoPage.ts
+++ MiTrabajoIvanRios/PracticaSaucedemo/src/pages/StepTwoPage.ts
@@ -7,19 +7,22 @@ export class StepTwoPage {
         this.page=page;
     }
     async calculateTotalPaymentAndTaxes(){
-        const cantidadItems =  await parseInt(this.page.locator("xpath=//div[@class='inventory_item_price']").toString());
-        console.log(cantidadItems);
-        const totalValue = await this.page.locator("xpath=//div[@class='summary_total_label']/text()[2]".toString());
-        console.log(totalValue);
+
+
+        let cantidadItems =  await this.page.locator("xpath=(//div[@class='inventory_item_price']/text()[2])[1]").innerText();
+        console.log(cantidadItems+" Cantidad de Items");
+        const totalValue =  await this.page.inputValue("xpath=//div[@class='summary_total_label']/text()[2]").toString();
+        console.log(totalValue+" Total Factura");
+
         let sumaTotal = 0;
-        for (let i=0;i<cantidadItems;i++){
-            sumaTotal += await parseInt(this.page.locator("xpath=//div[@class='inventory_item_price']/text()[2]").toString());
-        }
+       /* for (let i=0;i<cantidadItems;i++){
+            sumaTotal += parseFloat(this.page.locator("xpath=//div[@class='inventory_item_price']/text()[2]").toString());
+        }*/
         console.log(sumaTotal);
         let taxes = (sumaTotal*0.08).toFixed(2);
         console.log(taxes);
         let totalPayment = sumaTotal + taxes;
         console.log(totalPayment);
-        await expect(totalValue.toString()).toMatch(totalPayment.toString());
+        await expect(totalValue).toMatch(totalPayment.toString());
     }
 }
---
+++ ScreenPlayTest/lib/src/test/java/com/sophos/semillero/runners/Test1.java
@@ -0,0 +1,15 @@
+package com.sophos.semillero.runners;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+		glue = {"com.sophos.semillero.stepdefinitions"},
+		features = {"src/test/resources/com/sophos/semillero/features/test1.feature"})
+
+public class Test1 {
+
+}
