13eb3df43ba876dd7ef512b55ee9848b14860abc
Carmelo Jose Argel Mestra <carmelo.argel@sophossolutions.com>
2022-07-15 18:15:45 -0500

    Practica PhpTravel Playwright | Listar vuelo de menor precio

--- MiTrabajoCarmeloArgel/PhpTravelPractica/tests/testFlight.spec.ts
+++ MiTrabajoCarmeloArgel/PhpTravelPractica/tests/testFlight.spec.ts
@@ -6,17 +6,17 @@ import { FlightList } from '../src/pages/flightList';
 
 test(`
 Given the user was logged successfull
-
-When the user add item to card
-
+When the user add item to car
 Then the user saw the item in the car`,
 
   async ({ page }) => {
 
+    // Creamos objetos de cada clase.
     const homePhpTravel = new HomePhpTravel(page);
     const flightMedCan = new FlightMedCan(page);
     const flightList = new FlightList(page);
- 
+    
+    // Llamamos cada metodo a traves del objeto.
     await homePhpTravel.homePhpTravel();
     await flightMedCan.flight();
     await flightList.list();
